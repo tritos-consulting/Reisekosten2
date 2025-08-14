8e264bfa3c23c94f31f8ecbdfb5522639695df87
tritos-consulting <rechnungen.briefe@gmail.com>
2025-08-14 12:25:32 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
 export default defineConfig({
   plugins: [react()],
-  base: repo ? `/${repo}/` : '/', // passt sich automatisch an, z.B. /Reisekosten2/
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: repo ? `/${repo}/` : '/', // => /Reisekosten2/ auf GitHub Pages
+  build: { outDir: 'dist', sourcemap: false }
 })
