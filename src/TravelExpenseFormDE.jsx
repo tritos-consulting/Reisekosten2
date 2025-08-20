4ce5b181ab466ee9822f6a73836e5ca4d5a71df5
tritos-consulting <rechnungen.briefe@gmail.com>
2025-08-20 23:14:20 +0200

    Update TravelExpenseFormDE.jsx

--- src/TravelExpenseFormDE.jsx
+++ src/TravelExpenseFormDE.jsx
@@ -314,7 +314,7 @@ export default function TravelExpenseFormDE() {
 
   // ---------- State ----------
   const [basis, setBasis] = useState({
-    name: "Kromer Tobias",
+    name: "",
     zweck: "",
     beginn: "",
     ende: "",
